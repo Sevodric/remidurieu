0680f774c00c468f65f637499996d1945af2c8c3
Rémi Durieu <remi.durieu@univ-rouen.fr>
2020-05-03 12:48:51 +0200

    Fix incorrect English

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@
 /*
  * Adds an event to #arrow-down that scrolls down to #projects.
  */
-let arrow = document.getElementById('arrow-down')
-let target = document.getElementById('projects')
+const arrow = document.getElementById('arrow-down')
+const target = document.getElementById('projects')
 arrow.addEventListener('click', function() {
   target.scrollIntoView({
     behavior: 'smooth'
